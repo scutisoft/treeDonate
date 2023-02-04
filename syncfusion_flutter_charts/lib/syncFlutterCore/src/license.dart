2196595a3a41fe64aa28e9800a7d23810e2ae538
BalaSubramaniyan <76891599+bala-619@users.noreply.github.com>
2023-02-04 19:25:22 +0530

    dashboard

---
+++ syncfusion_flutter_charts/lib/syncFlutterCore/src/license.dart
@@ -0,0 +1,21 @@
+
+
+//ignore: avoid_classes_with_only_static_members
+import 'package:flutter/material.dart';
+
+/// The class for license validation.
+@Deprecated('License registration is not required now')
+class SyncfusionLicense {
+  /// <summary>
+  /// Method to validate the license and display popup.
+  /// </summary>
+  /// <param name="context">context</param>
+  // ignore: missing_return
+  static String? validateLicense(BuildContext context) {}
+
+  /// <summary>
+  /// Method to register the license key.
+  /// </summary>
+  /// <param name="licenseKey">licenseKey</param>
+  static void registerLicense(String licenseKey) {}
+}
